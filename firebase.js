b2262c427b516dd390d97e7ec6dcf29ee874ef54
joabeplay1 <joabejonatas186@gmail.com>
2026-05-16 18:01:14 -0400

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "SUA_KEY_AQUI",
+    authDomain: "SEU_DOMINIO_AQUI",
+    projectId: "SEU_PROJECT_ID_AQUI",
+    storageBucket: "SEU_STORAGE_BUCKET_AQUI",
+    messagingSenderId: "SEU_SENDER_ID_AQUI",
+    appId: "SEU_APP_ID_AQUI"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
