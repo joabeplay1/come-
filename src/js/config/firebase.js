cb06db6fa7e867b18f366f3f5a25ad87e821c56c
joabeplay1 <joabejonatas186@gmail.com>
2026-05-17 14:07:20 -0300

    firebase.js

--- src/js/config/firebase.js
+++ src/js/config/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/1
 import { getDatabase, ref, set, onValue, update } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZ4rOliexofYP8vyRLzUeX3mf5uXG6WRM",
-  authDomain: "aposta-96213.firebaseapp.com",
-  projectId: "aposta-96213",
-  storageBucket: "aposta-96213.firebasestorage.app",
-  messagingSenderId: "989060185373",
-  appId: "1:989060185373:web:69bb80b2f961fe8e9d35f4"
+    apiKey: "AIzaSyCZ4rOliexofYP8vyRLzUeX3mf5uXG6WRM",
+    authDomain: "aposta-96213.firebaseapp.com",
+    projectId: "aposta-96213",
+    storageBucket: "aposta-96213.firebasestorage.app",
+    messagingSenderId: "989060185373",
+    appId: "1:989060185373:web:69bb80b2f961fe8e9d35f4"
 };
 
 const app = initializeApp(firebaseConfig);
